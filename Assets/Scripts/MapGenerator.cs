b7b055fb79ec0e8141788a2462d8aca0d199edaf
Mugsend <chaudharysaurabh93063@gmail.com>
2024-11-27 11:56:23 +0530

    add colours for different map heights

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,6 +1,9 @@
+using Unity.Collections;
 using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode {NoiseMap, ColourMap};
+    public DrawMode drawMode;
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -11,11 +14,27 @@ public class MapGenerator : MonoBehaviour
     public int seed;
     public Vector2 offset;
     public bool autoUpdate;
+    public TerrainType[] regions;
     public void GenerateMap(){
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth,mapHeight, seed,noiseScale, octaves, persistance, lacunarity, offset);
-
+        Color[] colourMap = new Color[mapWidth*mapHeight];
+        for (int y=0;y<mapHeight;y++){
+            for (int x=0;x<mapHeight;x++){
+            float currentHeight = noiseMap[x,y];
+            for (int i=0;i<regions.Length;i++){
+                if (currentHeight<=regions[i].height){
+                    colourMap[y*mapWidth+x] = regions[i].colour;
+                    break;
+                }
+            }
+            }
+        }
         MapDisplay display = FindFirstObjectByType<MapDisplay> ();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap){
+display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap){}
+        display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
     }
     void OnValidate(){
         if (mapWidth<1){
@@ -31,4 +50,12 @@ public class MapGenerator : MonoBehaviour
             octaves=0;
         }
     }
+    
 }
+
+[System.Serializable]
+    public struct TerrainType{
+        public string name;
+        public float height;
+        public Color colour;
+    }
